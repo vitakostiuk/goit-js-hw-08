38648ff7280b505d1c2a1c933f899592992e1311
Vita Kostiuk <vitagrebennik@gmail.com>
2022-01-26 16:48:33 +0200

    task-3-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,11 +10,12 @@ let savedDataParsed = {};
 const savedData = localStorage.getItem(keyForStorage);
 if (savedData) {
     savedDataParsed = JSON.parse(savedData);
-    data = savedDataParsed;
-    formRef.elements.email.value = savedDataParsed.email ?? '';
-    formRef.elements.message.value = savedDataParsed.message ?? '';
 }
-
+    
+data = savedDataParsed;
+formRef.elements.email.value = savedDataParsed.email ?? '';
+formRef.elements.message.value = savedDataParsed.message ?? '';
+  
 const onInputForm = evt => {
   data = {
   ...data,
@@ -23,29 +24,28 @@ const onInputForm = evt => {
 
   const dataJson = JSON.stringify(data);
   localStorage.setItem(keyForStorage, dataJson);
-  console.log(dataJson);
 }
 
-// const onSubmitForm = evt => {
-//   evt.preventDefault();
+const onSubmitForm = evt => {
+  evt.preventDefault();
   
-//   const {
-//     elements: { email, message }
-//   } = evt.currentTarget;
+  const {
+    elements: { email, message }
+  } = evt.currentTarget;
   
-//   if (email.value === '' || message.value === '') {
-//     return;
-//   }
-//   const finalData = {
-//     email: formRef.elements.email.value,
-//     message: formRef.elements.message.value,
-//   }
-//     console.log(finalData);
+  if (email.value === '' || message.value === '') {
+    return;
+  }
+  const finalData = {
+    email: formRef.elements.email.value,
+    message: formRef.elements.message.value,
+  }
+    console.log(finalData);
   
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(keyForStorage);
-//   data = {};
-// }
+  evt.currentTarget.reset();
+  localStorage.removeItem(keyForStorage);
+  data = {};
+}
 
 formRef.addEventListener('input', throttle(onInputForm, 500));
-// formRef.addEventListener('submit', onSubmitForm);
+formRef.addEventListener('submit', onSubmitForm);
