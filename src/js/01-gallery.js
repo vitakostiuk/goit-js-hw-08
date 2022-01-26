ccc5bac6f139a0e6096d0b39d905a44fefa0f676
Vita Kostiuk <vitagrebennik@gmail.com>
2022-01-26 15:02:18 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainerRef.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryMarkup(items) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          title="${description}"
+        />
+      </a>
+`
+  }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+}
+);
+console.log(lightbox);
